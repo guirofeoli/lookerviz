6e92c56dfd471bd498d5a1cf0926cb856abbe8d9
guirofeoli <guirfo@gmail.com>
2025-06-04 15:12:34 -0300

    Update and rename index.js to index.html

--- index.js
+++
@@ -1,8 +0,0 @@
-function drawViz(data, element) {
-  element.innerHTML = '';
-  var div = document.createElement('div');
-  div.style.fontSize = '24px';
-  div.style.fontFamily = 'Arial';
-  div.innerText = 'Total de linhas: ' + data.tables.DEFAULT.length;
-  element.appendChild(div);
-}
